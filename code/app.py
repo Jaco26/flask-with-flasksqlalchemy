5be5150bb27a6965fc1ef7f5992a967b2e603386
Jacob Albright <jacob.albright23@gmail.com>
2018-10-25 20:12:24 -0500

    created models and resources packages

--- code/app.py
+++ code/app.py
@@ -3,22 +3,13 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from user import UserRegister
-from item import Item, ItemList
+from resources.user import UserRegister
+from resources.item import Item, ItemList
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
 api = Api(app)
 
-#   JWT creates a new endpoint ('/auth')
-# When we call /auth, we send a username and password.
-# the JWT extension passes that data to the 'authenticate'
-# method we imported from our module 'security.py'
-# If username and password are legit, /auth returns a JWT which reprensents
-# a user's identity
-#   We can send that JWT back to a user and when they send further requests
-# to other endpoints, JWT uses the 'identity' method imported from 'security.py'
-# to get that user's information.
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>') # make the Item resource accessible from the api at the route provided as the second argument
--- code/security.py
+++ code/security.py
@@ -1,18 +1,7 @@
 from werkzeug.security import safe_str_cmp
-from user import User
+from resources.user import User
 
 # werkzeug.security safe_str_cmp accounts for different String encodings before comparisons (eg. ascii, utf-8)
-
-# registered users table
-users = [
-  User(1, 'bob', 'asdf'),
-  User(2, 'caroline', 'fdsa'),
-]
-
-# username mapping
-username_mapping = { u.username: u for u in users } # set comprehension !!! COOL
-# user id mapping
-userid_mapping = { u.id: u for u in users }
 
 def authenticate(username, password):
   user = User.find_by_username(username)
